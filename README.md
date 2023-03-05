4f88e74698f406d1fdc755b5fcd047e76ac09b1a
proffix4 <proffix4@gmail.com>
2023-03-05 23:36:42 +0600

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,25 @@
+# VS2022_CPP_wxWidgets_Demo
+Пример программы на C++ с использованием wxWidgets в Visual Studio 2022
+
+https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.2.1/wxWidgets-3.2.2.1.zip
+
+https://github.com/wxFormBuilder/wxFormBuilder/releases/download/v3.10.1/wxFormBuilder-3.10.1-x64.exe
+
+![srcreenshot](screenshot1.jpg)
+
+![srcreenshot](screenshot2.png)
+
+![srcreenshot](screenshot3.png)
+
+![srcreenshot](screenshot4.png)
+
+```
+$(wxwin)\include\msvc;$(wxwin)\include
+```
+
+![srcreenshot](screenshot5.png)
+```
+$(wxwin)\lib\vc_x64_lib
+```
+
+
---
+++ main.cpp
@@ -0,0 +1,39 @@
+﻿#include "main.h"
+
+#include <wx/msgdlg.h>
+
+// initialize the application
+IMPLEMENT_APP(MainApp);
+
+////////////////////////////////////////////////////////////////////////////////
+// application class implementation
+////////////////////////////////////////////////////////////////////////////////
+
+bool MainApp::OnInit() {
+	SetTopWindow(new MainFrame(NULL));
+	GetTopWindow()->Show();
+	// true = enter the main loop
+	return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// main application frame implementation
+////////////////////////////////////////////////////////////////////////////////
+
+MainFrame::MainFrame(wxWindow* parent)
+	: MainFrameBase(parent) {
+	wxString icon = wxT("tsnsoft.xpm");
+	if (wxFindFirstFile(icon) != "") {
+		wxInitAllImageHandlers();
+		this->SetIcon(wxIcon(icon, wxBITMAP_TYPE_XPM));
+	}
+}
+
+MainFrame::~MainFrame() {
+}
+
+// ЭТО НАША РЕАЛИЗАЦИЯ ОБРАБОТЧИКА ЗАКРЫТИЯ ФОРМЫ!
+void MainFrame::OnCloseFrame(wxCloseEvent& event) {
+	Destroy();
+}
+
